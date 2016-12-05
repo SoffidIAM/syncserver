debb2d4a0886d91a17c8aa1ab567560da4571897
Gabriel Buades <gbuades@soffid.com>
2016-12-05 16:06:50 +0100

    Dump log on task error

--- server/src/main/java/es/caib/seycon/ng/sync/engine/UpdateTaskStatusThread.java
+++ server/src/main/java/es/caib/seycon/ng/sync/engine/UpdateTaskStatusThread.java
@@ -23,12 +23,39 @@ public class UpdateTaskStatusThread extends Thread
         TaskQueue taskQueue = ServerServiceLocator.instance().getTaskQueue();
         while (true)
         {
+        	TaskHandler task = null;
         	try {
-        		TaskHandler task = taskQueue.peekTaskToPersist();
+        		task = taskQueue.peekTaskToPersist();
         		taskQueue.persistTask(task);
         	} catch (Throwable e)
         	{
-        		log.warn("Error on update tasks thread", e);
+       			log.warn("Error on update tasks thread", e);
+        		if (task != null && task.getTask() != null)
+        		{
+        			log.warn("Task details: "+task.getTask().toString());
+        			if (task.getLogs() != null)
+        			{
+        				for (TaskHandlerLog tl : task.getLogs())
+        				{
+        					StringBuffer sb = new StringBuffer();
+        					sb.append (">> ");
+        					if (tl.getDispatcher() != null && tl.getDispatcher().getDispatcher() != null)
+        						sb.append (tl.getDispatcher().getDispatcher().getCodi());
+        					else
+        						sb.append ("Unknown dispatcher");
+        					sb.append (": ");
+        					if (tl.isComplete())
+        						sb.append ("DONE");
+        					else 
+        					{
+        						sb.append ("ERROR ")
+        							.append (tl.getReason());
+        					}
+        							
+        					log.warn (sb.toString());
+        				}
+        			}
+        		}
         		try {
 					Thread.sleep(2000);
 				} catch (InterruptedException e1) {
