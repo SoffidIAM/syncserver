af6cbbdf0e883a3066dc43c7a866376a23d66c12
Gabriel Buades <gbuades@soffid.com>
2018-05-27 22:08:39 +0200

    Fix reconcile role process

--- server/src/main/java/com/soffid/iam/sync/engine/ReconcileEngine2.java
+++ server/src/main/java/com/soffid/iam/sync/engine/ReconcileEngine2.java
@@ -606,7 +606,7 @@ public class ReconcileEngine2
 					if (r != null)
 					{
 						r.setName(roleName);
-						createRole(r);
+						loadRole(r);
 					}
 				} else {
 					Watchdog.instance().interruptMe(dispatcher.getLongTimeout());
