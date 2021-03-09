8672b2931560b59c1b8250a05890f97ad536a95d
Gabriel Buades <gbuades@soffid.com>
2021-03-09 15:27:43 +0100

    Store information in kubernetes secret

---
+++ bootstrap/src/main/java/com/soffid/iam/sync/bootstrap/KubernetesSaver.java
@@ -0,0 +1,17 @@
+package com.soffid.iam.sync.bootstrap;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import com.soffid.iam.sync.bootstrap.impl.KubernetesConfig;
+
+public class KubernetesSaver {
+	public static void main(String args[]) throws KeyManagementException, UnrecoverableKeyException, FileNotFoundException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+		new KubernetesConfig().save();
+	}
+}
