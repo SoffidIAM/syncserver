627d967d25ca2aa28d96745e151b1da92b9bcacc
Gabriel Buades <gbuades@soffid.com>
2022-04-13 12:26:36 +0200

    Update kubernetes configuration after renewing certificates

--- server/src/main/java/com/soffid/iam/sync/engine/cert/UpdateCertsTask.java
+++ server/src/main/java/com/soffid/iam/sync/engine/cert/UpdateCertsTask.java
@@ -6,6 +6,7 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.cert.X509Certificate;
 import java.util.Date;
+import java.util.Random;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -16,6 +17,7 @@ import org.bouncycastle.asn1.x500.style.RFC4519Style;
 import com.soffid.iam.api.Password;
 import com.soffid.iam.config.Config;
 import com.soffid.iam.ssl.SeyconKeyStore;
+import com.soffid.iam.sync.tools.KubernetesConfig;
 import com.soffid.iam.utils.Security;
 
 import es.caib.seycon.ng.remote.RemoteServiceLocator;
@@ -28,7 +30,11 @@ public class UpdateCertsTask implements Runnable {
 	public void run() {
 		do {
 	        try {
+	        	try {
+	        		Thread.sleep(new Random().nextInt(15 * 60 * 1000));
+	        	} catch (InterruptedException e) {}
 	        	Config config = Config.getConfig();
+	        	new KubernetesConfig().load();
 				KeyStore ks = SeyconKeyStore.loadKeyStore(SeyconKeyStore.getKeyStoreFile());
 				Password password = SeyconKeyStore.getKeyStorePassword();
 				X509Certificate cert = (X509Certificate) ks.getCertificate(SeyconKeyStore.MY_KEY);
@@ -49,6 +55,7 @@ public class UpdateCertsTask implements Runnable {
 			            new RemoteServiceLocator().getServerService().addCertificate(newCert);
 			            ks.setKeyEntry(SeyconKeyStore.NEXT_CERT, privateKey, password.getPassword().toCharArray(), new X509Certificate[] {newCert});
 			            SeyconKeyStore.saveKeyStore(ks, SeyconKeyStore.getKeyStoreFile());
+			        	new KubernetesConfig().save();
 			        }
 				}
 				if (cert.getNotAfter().getTime() < now + oneDay * 15) {
