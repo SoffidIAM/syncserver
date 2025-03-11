d98ecf9c089c872de70419927c0f36c85d3cacaa
Gabriel Buades <gbuades@soffid.com>
2025-03-11 15:09:13 +0100

    Upgrade Oracle drivers

--- server/src/main/docker/start.sh
+++ server/src/main/docker/start.sh
@@ -167,7 +167,7 @@ function loadconfig {
 	  java=java
 	fi
 	
-	$java -cp "/opt/soffid/iam-sync/bin/bootstrap.jar:/opt/soffid/iam-sync/lib/mariadb-java-client-1.8.0.jar:/opt/soffid/iam-sync/lib/ojdbc10-19.18.0.0.jar:/opt/soffid/iam-sync/lib/postgresql-42.2.5.jre7.jar:/opt/soffid/iam-sync/lib/sqljdbc4-3.0.jar" com.soffid.iam.sync.bootstrap.KubernetesLoader
+	$java -cp "/opt/soffid/iam-sync/bin/bootstrap.jar:/opt/soffid/iam-sync/lib/mariadb-java-client-1.8.0.jar:/opt/soffid/iam-sync/lib/ojdbc10-19.20.0.0.jar:/opt/soffid/iam-sync/lib/postgresql-42.2.5.jre7.jar:/opt/soffid/iam-sync/lib/sqljdbc4-3.0.jar" com.soffid.iam.sync.bootstrap.KubernetesLoader
 }
 
 loadconfig
